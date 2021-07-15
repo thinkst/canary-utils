980b1b98b5a56994ed853d195692b003fe5f61de
Mr Forest <1726833+0xForest@users.noreply.github.com>
2021-07-15 08:50:21 +0200

    Update Invoke-CreateCanarytokens.ps1
    Simplified commenting.

--- Invoke-CreateCanarytokens.ps1
+++ Invoke-CreateCanarytokens.ps1
@@ -1,152 +1,111 @@
 <#
 .SYNOPSIS
-    Creates Canarytokens and drops them on a list of remote windows hosts specified in a hosts.txt file,
-
+    Creates Canarytokens and drops them to local host. Uses Canarytoken Factory, so it can be safely used for mass deployment without revealing your API key.
 .NOTES
-    For this tool to work, you must have your Canary Console API enabled, please 
-    follow this link to learn how to do so:
-    https://help.canary.tools/hc/en-gb/articles/360012727537-How-does-the-API-work-
+    You will need your API enabled on your Console, A guide to enable it is available here. https://help.canary.tools/hc/en-gb/articles/360012727537-How-does-the-API-work-
 
     ###################
     How does this work?
     ###################
-    1. Create a 'hosts.txt' file which contains the target hosts where you want to
-    drop the Canarytokens 'hostname or IP', each host in a line.
-    2. Place 'hosts.txt' in the same directory as the script.
-    3. Run powershell as a user that has remote read/write access on the admin shares 
-    of the remote hosts 'namely C$' ... either log in as such user, or use `runas /user:...`
-    4. Provide your Console domain + API auth ... the tool will run.
+    1. Create the flock you want the tokens to be part of in your console.
+    2. Get the Flock ID (https://docs.canary.tools/flocks/queries.html#list-flocks-summary)
+    3. Create a Canarytoken Factory (https://docs.canary.tools/canarytokens/factory.html#create-canarytoken-factory-auth-string)
+    4. Make sure the host has access to the internet.
+    5. Run powershell as a user that has read/write access on the target directory.
     
-    Last Edit: 2021-01-25
-    Version 1.0 - initial release
-
+    Last Edit: 2021-07-15
+    Version 1.1 - Condensed Instructions
 .EXAMPLE
-    .\Invoke-CreateCanarytokens.ps1
-    This will run the tool with the default params
+    .\Invoke-CreateCanarytokensFactoryLocal.ps1
+    This will run the tool with the default params, asking interactively for missing ones.
 
-    .\Invoke-CreateCanarytokens.ps1 -TargetDirectory secret -TokenType aws-id -TokenFilename aws_secret.txt
+    .\Invoke-CreateCanarytokensFactoryLocal.ps1 -Domain aabbccdd.canary.tools -FactoryAuth XXYYZZ -TargetDirectory "c:\secret" -TokenType aws-id -TokenFilename aws_secret.txt
     creates an AWS-ID Canarytoken, using aws_secret.txt as the filename, and place it under c:\secret
 #>
 
 Param (
-    # Set the target Directory on hosts' root
-    # tokens will be dropped at 'c:\$TargetDirectory'
-    # e.g. 'c:\Backup'
-    # will be created if not exists
-    [string]$TargetDirectory = "Backup",
-
-    # Valid TokenType are as follows:
-    #   "aws-id":"Amazon API Key",
-    #   "doc-msword":"MS Word .docx Document",
-    #   "msexcel-macro":"MS Excel .xlsm Document",
-    #   "msword-macro":"MS Word .docm Document",
-    #   "pdf-acrobat-reader":"Acrobat Reader PDF Document",
-    # if you change $TokenType, make sure to pick an appropriate filename extension in next line
-    [string]$TokenType = 'doc-msword' ,
-    [string]$TokenFilename = "credentials.docx",
-
-    # Hosts file should contain the hosts on which tokens will be dropped.
-    # Each host in one line.
-    # The user that invoked the script should be able to map and write to admin shares
-    # on those hosts.
-    [string]$HostsFile = 'hosts.txt'
+    [string]$Domain = '', # Enter your Console domain between the quotes. e.g. 1234abc.canary.tools
+    [string]$FactoryAuth = '', # Enter your Factory auth key. e.g a1bc3e769fg832hij3 Docs available here. https://docs.canary.tools/canarytokens/factory.html#create-canarytoken-factory-auth-string
+    [string]$FlockID = 'flock:default', # Enter desired flock to place tokens in. This mis required. Docs available here. https://docs.canary.tools/flocks/queries.html#list-flock-sensors
+    [string]$TargetDirectory = "c:\Backup", # Local location to drop the token into. This will be created if it does not exist.
+    [string]$TokenType = 'doc-msword' , # Enter your desired token type. Full list available here. https://docs.canary.tools/canarytokens/factory.html#list-canarytokens-available-via-canarytoken-factoryif
+    [string]$TokenFilename = "credentials.docx" # Desired Token file name. Make sure to pick an appropriate filename extension in next line.
 )
 
 # We force TLS1.2 since our API doesn't support lower.
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 Set-StrictMode -Version 2.0
 
-# Does the file exist?
-if (-not (Test-Path $HostsFile)) {
-    Write-Error -ErrorAction Stop "[X] The file `'$HostsFile`' does not exist!
-    ... make sure you create a file that has all the target hosts in it, each host in one line. " 
-}
-
 # Connect to API
 # Get Console Domain
 $ApiHost = [string]::Empty
-Do {
-    $ApiHost = Read-Host -Prompt "[+] Enter your Full Canary domain (e.g. 'xyz.canary.tools')"
-} Until (($ApiHost.Length -gt 0) -and ([System.Net.Dns]::GetHostEntry($ApiHost).AddressList[0].IPAddressToString))
+if ($Domain -ne '') {
+    $ApiHost = $Domain
+} else {
+    Do {
+        $ApiHost = Read-Host -Prompt "[+] Enter your Full Canary domain (e.g. 'xyz.canary.tools')"
+    } Until (($ApiHost.Length -gt 0) -and ([System.Net.Dns]::GetHostEntry($ApiHost).AddressList[0].IPAddressToString))
+}
 
 # Get API Auth Token
-$ApiTokenSecure = New-Object System.Security.SecureString
-Do {
-    $ApiTokenSecure = Read-Host -AsSecureString -Prompt "[+] Enter your Canary API key"
-} Until ($ApiTokenSecure.Length -gt 0)
-$ApiToken = (New-Object System.Management.Automation.PSCredential "user", $ApiTokenSecure).GetNetworkCredential().Password
+$ApiToken = [string]::Empty
+if ($FactoryAuth -ne '') {
+    $ApiToken = $FactoryAuth
+} else {
+    $ApiTokenSecure = New-Object System.Security.SecureString
+    Do {
+        $ApiTokenSecure = Read-Host -AsSecureString -Prompt "[+] Enter your Canary Factory Auth key"
+    } Until ($ApiTokenSecure.Length -gt 0)
+    $ApiToken = (New-Object System.Management.Automation.PSCredential "user", $ApiTokenSecure).GetNetworkCredential().Password
+}
 
 Write-Host -ForegroundColor Green "[*] Starting Script with the following params:
         Console Domain   = $ApiHost
+        Flock ID         = $FlockID
         Target Directory = $TargetDirectory 
         Token Type       = $TokenType
         Token Filename   = $TokenFilename
 "
 
 $ApiBaseURL = '/api/v1'
-Write-Host -ForegroundColor Green "[*] Pinging Console..."
 
-$PingResult = Invoke-RestMethod -Method Get -Uri "https://$ApiHost$ApiBaseURL/ping?auth_token=$ApiToken"
-$Result = $PingResult.result
+Write-Host -ForegroundColor Green "[*] Checking if '$TargetDirectory' exists..."
+
+# Creates the target directory if it does not exist
+If (!(Test-Path $TargetDirectory)) {
+    Write-Host -ForegroundColor Green "[*] '$TargetDirectory' doesn't exist, creating it ..."
+    New-Item -ItemType Directory -Force -Verbose -ErrorAction Stop -Path "$TargetDirectory"
+}
+# Check whether token file already exists on the local machine
+$OutputFileName = "$TargetDirectory\$TokenFilename"
+Write-Host -ForegroundColor Green "[*] Dropping '$OutputFileName' ..."
+
+If (Test-Path $OutputFileName) {
+    Write-Host Skipping $OutputFileName, file already exists.
+    Continue        
+}
+
+# Create token on Console
+$TokenName = $OutputFileName
+$PostData = @{
+    factory_auth = "$ApiToken"
+    kind       = "$TokenType"
+    flock_id = "$FlockID"
+    memo       = "$([System.Net.Dns]::GetHostName()) - $TokenName"
+}
+Write-Host -ForegroundColor Green "[*] Hitting API to create token ..."
+$CreateResult = Invoke-RestMethod -Method Post -Uri "https://$ApiHost$ApiBaseURL/canarytoken/factory/create" -Body $PostData
+$Result = $CreateResult.result
 If ($Result -ne 'success') {
-    Write-Host -ForegroundColor Red "[X] Cannot ping Canary API. Bad token?"
+    Write-Host -ForegroundColor Red "[X] Creation of $TokenName failed."
     Exit
 }
 Else {
-    Write-Host -ForegroundColor Green "[*] Canary API available for service!"
+    $TokenID = $($CreateResult).canarytoken.canarytoken
+    Write-Host -ForegroundColor Green "[*] Token Created (ID: $TokenID)."
 }
 
-# Getting content of the Targets TXT file
-# this should have a list of Targets' host names, each on its own line.
-Write-Host -ForegroundColor Green "[+] Reading targets' Hostnames from '$HostsFile'"
-$TargetsText = Get-Content $HostsFile -ErrorAction Stop
-
-
-# convert the file content to an array, skipping empty lines
-$Targets = $($TargetsText -split "`n").Where( { $_.Trim() -ne "" })
-Write-Host -ForegroundColor Green "[*] Found a total number of '$($Targets.length)' targets..."
-
-ForEach ($TargetHostname in $Targets) {
-    Write-Host -ForegroundColor Green "[*] Working with '$TargetHostname' ..."
-
-    $NetworkPath = "\\$TargetHostname\C`$\$TargetDirectory"
-    Write-Host -ForegroundColor Green "[*] Checking if '$NetworkPath' exists..."
-
-    # Create the target Dir if not exist
-    If (!(Test-Path $NetworkPath)) {
-        Write-Host -ForegroundColor Green "[*] '$NetworkPath' doesn't exist, creating it ..."
-        New-Item -ItemType Directory -Force -Verbose -ErrorAction Stop -Path "$NetworkPath"
-    }
-    # Check whether token already exists
-    $OutputFileName = "$NetworkPath\$TokenFilename"
-    Write-Host -ForegroundColor Green "[*] Dropping '$OutputFileName' ..."
-
-    If (Test-Path $OutputFileName) {
-        Write-Host Skipping $TargetHostname, file already exists.
-        Continue        
-    }
-
-    # Create token
-    $TokenName = $OutputFileName
-    $PostData = @{
-        auth_token = "$ApiToken"
-        kind       = "$TokenType"
-        memo       = "$TargetHostname - $TokenName"
-    }
-    Write-Host -ForegroundColor Green "[*] Hitting API to create token ..."
-    $CreateResult = Invoke-RestMethod -Method Post -Uri "https://$ApiHost$ApiBaseURL/canarytoken/create" -Body $PostData
-    $Result = $CreateResult.result
-    If ($Result -ne 'success') {
-        Write-Host "Creation of $TokenName failed."
-        Exit
-    }
-    Else {
-        $WordTokenID = $($CreateResult).canarytoken.canarytoken
-        Write-Host -ForegroundColor Green "[*] Token Created (ID: $WordTokenID)."
-    }
-
-    # Download token
-    Write-Host -ForegroundColor Green "[*] Downloading Token from Console..."
-    Invoke-RestMethod -Method Get -Uri "https://$ApiHost$ApiBaseURL/canarytoken/download?auth_token=$ApiToken&canarytoken=$WordTokenID" -OutFile "$OutputFileName"
-    Write-Host -ForegroundColor Green "[*] Token Successfully written to destination: '$OutputFileName'."
-}
+# Downloads token and places it in the destination folder.
+Write-Host -ForegroundColor Green "[*] Downloading Token from Console..."
+Invoke-RestMethod -Method Get -Uri "https://$ApiHost$ApiBaseURL/canarytoken/factory/download?factory_auth=$ApiToken&canarytoken=$TokenID" -OutFile "$OutputFileName"
+Write-Host -ForegroundColor Green "[*] Token Successfully written to destination: '$OutputFileName'."
