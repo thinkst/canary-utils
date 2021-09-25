1b464b6e62a97f419a77a23fba7aebe66b133f72
Justin Varner <61245735+justin-varner@users.noreply.github.com>
2021-09-25 00:25:35 -0400

    Adding Recent Events Script
    This script only pulls down the 100 most recent Canary events. Also fixing syntax with the other scripts.

---
+++ Recent-Canary-GreyNoise-Enterprise-Threat-Intel-Report.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This script pulls recent alert data from a Canary outside bird, sends the results to the GreyNoise Enterprise API for detailed threat intelligence context, and creates a JSON report.
+# Please consult the README for prequisites and usage instructions
+
+# Cleanup any residual files that may have been left behind from previously running the script
+#rm -rf outside_bird_alerts.json.zip outside-bird-$BIRD_ID.json canary-ips-*.txt
+
+curl -XGET "https://$CANARY_HASH.canary.tools/api/v1/incidents/outside_bird/search" \
+  -d auth_token=$CANARY_TOKEN \
+  -d node_ids=$BIRD_ID \
+  -G | jq '.src_ips[] | .ip_address' | sed 's/\"//g' | sort > canary-ips-$(date +%Y-%m-%d).txt
+
+#curl https://$CANARY_HASH.canary.tools/api/v1/incidents/outside_bird/search -d auth_token=$CANARY_TOKEN -d node_ids=000103c21e6928dd -G | jq '.src_ips[] | .ip_address' | sed 's/\"//g' | sort > canary-ips-$(date +%Y-%m-%d).txt
+
+# Read through each line of the canary text file, send each IP through the GreyNoise Enterprise API, and create a detailed JSON report
+file="canary-ips-$(date +%Y-%m-%d).txt"
+lines=$(cat $file)
+
+for line in $lines
+do
+    curl "https://api.greynoise.io/v2/noise/context/$line" -H "key: $GREYNOISE_TOKEN" -H "accept: application/json" | jq '.' | cat >> Recent-Canary-GreyNoise-Enterprise-Threat-Intel-Report-$(date +%Y-%m-%d).json
+done
