d427e52b6ff5a6b11c7f412e73f40c61cf5044e0
Xorlent <94985297+Xorlent@users.noreply.github.com>
2023-10-09 02:05:29 -0700

    Update Fetch-Canary-Syslog.ps1 (#36)
    * Update Fetch-Canary-Syslog.ps1
    
    Fixed single-line parse issue with simplified string -split regex.
    
    * Update canary-receiver.js
    
    Bug fix: Filtering IPs going to blocklist KV store based on event description.
    
    * Update canary-receiver.js
    
    * Update Fetch-Canary-Syslog.ps1
    
    Fixed MS issue with disabled mshtml renderer and Invoke-WebRequest
    
    * Update canary-receiver.js
    
    Refactored, updated event handler to include "Custom Service" events.
    
    * Update canary-receiver.js
    
    Now processes event data whether Syslog is enabled or not.
    
    * Update canary-receiver.js
    
    Moved all syslog-specific code to syslog parse/process section.
    
    * Update canary-receiver.js
    
    Changed MaliciousEvent variable to boolean

--- Cloudflare/Fetch-Canary-Syslog.ps1
+++ Cloudflare/Fetch-Canary-Syslog.ps1
@@ -1,32 +1,38 @@
 $ConfigFile = '.\Fetch-Canary-Syslog-Config.xml'
 $ConfigParams = [xml](get-content $ConfigFile)
- 
+
 # Initialize configuration variables from config xml file
 $WorkerURL = $ConfigParams.configuration.cloudflare.URL.value
 $WorkerAuth = $ConfigParams.configuration.cloudflare.auth.value
 $SyslogTarget = $ConfigParams.configuration.syslog.fqdn.value
 $SyslogPort = $ConfigParams.configuration.syslog.port.value
 $OutputFile = $ConfigParams.configuration.file.canarylogs.value
- 
+
 # Force TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
- 
-$Response = Invoke-WebRequest -Uri $WorkerURL -Headers @{ 'auth' = $WorkerAuth}
- 
+
+# Request the queued Canary events (-UseBasicParsing ensures we don't use the deprecated IE HTML renderer)
+$Response = Invoke-WebRequest -Uri $WorkerURL -Headers @{ 'auth' = $WorkerAuth} -UseBasicParsing
+
 if($Response.Statuscode -eq 204){exit 0} # Nothing to process - resultset is empty
- 
+
+# Prepare the Syslog UDP socket
+if ($SyslogTarget -ne "syslog.hostname.here") {
+   $UdpClient = New-Object System.Net.Sockets.UdpClient $SyslogTarget, $SyslogPort
+   }
+
 $Content = $Response.Content
-$SyslogArray = ($Content -split "\r?\n|\r")
+$SyslogArray = $Content.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries)
+
 ForEach ($SyslogEntry in $SyslogArray){
    # Convert message to array of ASCII bytes.
    $bytearray = $([System.Text.Encoding]::ASCII).getbytes($SyslogEntry)
-   
    # Send the Syslog message...
    if ($SyslogTarget -ne "syslog.hostname.here") {
-       $UdpClient = New-Object System.Net.Sockets.UdpClient $SyslogTarget, $SyslogPort
        $UdpClient.Send($bytearray, $bytearray.length) | out-null
    }
    else {
-       $SyslogEntry >> $OutputFile
+       # No syslog server was specified in the configuration file, so output to a local file
+       Add-Content $OutputFile $SyslogEntry
    }
 }
--- Cloudflare/canary-receiver.js
+++ Cloudflare/canary-receiver.js
@@ -1,94 +1,99 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-// Change this to a unique value -- this will become the authentication header field "auth" string for the Canary generic webhook to use.
-// Note: Be careful not to use the " or $ character in this authentication string
-const authString = "canhasauthenticated"
-
-// Syslog messages formatted to RFC 3164 (BSD)
-// Change true to false to disable the Syslog parse/store routine.
-const EnableSyslog = true
-// Ensures the KV records purge after 7 days if they have not been picked up for syslog processing.
-const SyslogBufferTTL = 604800
-
-// Change "BlockListCanary" to the name of a Canary you would like contributing to a IP blocklist.  Leave value as-is to disable.
-const MyCanary = "BlockListCanary"
-// Auto-purge blocked IP addresses after 6 months.
-const IPBlocklistTTL = 15552000
-
-const Months = ["zero", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-async function handleRequest(request) {
-  // check we have a POST request
-  if (request.method !== "POST") {    
-    return new Response(``, {status: 200}) 
-  }
-  try {
-    let Auth = request.headers.get("auth")
-    
-    // check to make sure the request is authenticated with a secret
-    if (Auth !== authString){
-      return new Response(``, {status: 200})
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  // Change this to a unique value -- this will become the authentication header field "auth" string for the Canary generic webhook to use.
+  // Note: Be careful not to use the " or $ character in this authentication string
+  const authString = "canhasauthenticated"
+  
+  // Syslog messages formatted to RFC 3164 (BSD)
+  // Change true to false to disable the Syslog parse/store routine.
+  const EnableSyslog = true
+  // Ensures the KV records purge after 7 days if they have not been picked up for syslog processing.
+  const SyslogBufferTTL = 604800
+  
+  // Change "BlockListCanary" to the name of a Canary you would like contributing to a IP blocklist.  Leave value as-is to disable.
+  const MyCanary = "BlockListCanary"
+  // Auto-purge blocked IP addresses after 6 months.
+  const IPBlocklistTTL = 15552000
+  
+  const Months = ["zero", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  
+  async function handleRequest(request) {
+    // check we have a POST request
+    if (request.method !== "POST") {    
+      return new Response(``, {status: 200}) 
     }
-    else {
-      // authentication received, process event
-      const data = await request.json()
-      let Timestamp = data.Timestamp
-      let Payload = "n/a"
-
-      if (EnableSyslog) {
-        let SyslogSplitDate = Timestamp.split("-")
-        let SyslogSplitTime = SyslogSplitDate[2].split(" ")
-        let MonthIndex = Number(SyslogSplitDate[1])
-        let SyslogMonth = Months[MonthIndex]
-        SyslogSplitTime[0] = SyslogSplitTime[0].replace("0"," ")
-        let SyslogPriority = "<113>"
-        switch (true){
-          case /Scan/.test(data.Description):
-            SyslogPriority = "<116>";
-            break;
-          case /Load/.test(data.Description):
-            SyslogPriority = "<116>";
-            break;
-          case /Settings Changed/.test(data.Description):
-            SyslogPriority = "<117>";
-            break;
-          case /Disconnected/.test(data.Description):
-            SyslogPriority = "<114>";
-            break;
-          case /Reconnected/.test(data.Description):
-            SyslogPriority = "<118>";
-            break;
-          case /Dummy/.test(data.Description):
-            SyslogPriority = "<119>";
-            break;
-        }
-
-        Payload = `${SyslogPriority} ${SyslogMonth} ${SyslogSplitTime[0]} ${SyslogSplitDate[0]} ${SyslogSplitTime[1]} ${data.CanaryName}(${data.CanaryLocation}) CanaryEvent: ${data.Intro} | Desc: ${data.Description} | Port: ${data.CanaryPort} | RemoteIP: ${data.SourceIP} | PTR: ${data.ReverseDNS}`
-        // Store in KV store (Key-Value store)
-        await canaryevents.put(Payload, Timestamp, {expirationTtl: SyslogBufferTTL})
-      }
-
-      if (data.CanaryName == MyCanary) {
-        const MaliciousIP = data.SourceIP
-        // process  
-        // Store in KV store (Key-Value store)
-        await canaryblocks.put(MaliciousIP, Timestamp, {expirationTtl: IPBlocklistTTL})
-        return new Response(
-          `Marked IP: ${MaliciousIP} and Stored Event: ${Payload}`,
-          {status: 200}
-        )
+    try {
+      let Auth = request.headers.get("auth")
+      
+      // check to make sure the request is authenticated with a secret
+      if (Auth !== authString){
+        return new Response(``, {status: 200})
       }
       else {
-      return new Response(
-        `Stored Event: ${Payload}`,
-        {status: 200}
-        )
+        // authentication received, process event
+        const data = await request.json()
+        let Timestamp = data.Timestamp
+        let Payload = "n/a"
+        let MaliciousEvent = true
+        let SyslogPriority = "<113>"
+        switch (true){
+            case /Scan/.test(data.Description):
+              SyslogPriority = "<116>";
+              break;
+            case /Custom/.test(data.Description):
+              SyslogPriority = "<116>";
+              break;
+            case /Load/.test(data.Description):
+              SyslogPriority = "<116>";
+              break;
+            case /Settings Changed/.test(data.Description):
+              SyslogPriority = "<117>";
+              MaliciousEvent = false;
+              break;
+            case /Disconnected/.test(data.Description):
+              SyslogPriority = "<114>";
+              MaliciousEvent = false;
+              break;
+            case /Reconnected/.test(data.Description):
+              SyslogPriority = "<118>";
+              MaliciousEvent = false;
+              break;
+            case /Dummy/.test(data.Description):
+              SyslogPriority = "<119>";
+              MaliciousEvent = false;
+              break;
+        }
+        if (EnableSyslog) {
+          let SyslogSplitDate = Timestamp.split("-")
+          let SyslogSplitTime = SyslogSplitDate[2].split(" ")
+          let MonthIndex = Number(SyslogSplitDate[1])
+          let SyslogMonth = Months[MonthIndex]
+          SyslogSplitTime[0] = SyslogSplitTime[0].replace("0"," ")
+          Payload = `${SyslogPriority} ${SyslogMonth} ${SyslogSplitTime[0]} ${SyslogSplitDate[0]} ${SyslogSplitTime[1]} ${data.CanaryName}(${data.CanaryLocation}) CanaryEvent: ${data.Intro} | Desc: ${data.Description} | Port: ${data.CanaryPort} | RemoteIP: ${data.SourceIP} | PTR: ${data.ReverseDNS}`
+          // Store in KV store (Key-Value store)
+          await canaryevents.put(Payload, Timestamp, {expirationTtl: SyslogBufferTTL})
+        }
+  
+        if (data.CanaryName == MyCanary && MaliciousEvent) {
+          const MaliciousIP = data.SourceIP
+          // Store in KV store (Key-Value store)
+          await canaryblocks.put(MaliciousIP, Timestamp, {expirationTtl: IPBlocklistTTL})
+          return new Response(
+            `Marked IP: ${MaliciousIP} and Stored Event: ${Payload}`,
+            {status: 200}
+          )
+        }
+        else {
+          return new Response(
+          `Stored Event: ${Payload}`,
+          {status: 200}
+          )
+        }
       }
+    } catch (e) {
+      /* use `Error:  ${e}` response string to debug */
+      return new Response(``, {status: 200})
     }
-  } catch (e) {
-    /* use `Error:  ${e}` response string to debug */
-    return new Response(``, {status: 200})
   }
-}
