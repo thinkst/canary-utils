a0fbd5723dc289d070630321c5ab9820255a5cae
thinkst-salman <142021049+thinkst-salman@users.noreply.github.com>
2023-10-24 17:53:30 +0530

    Update multi-dropper scripts (#38)
    * Update CanaryToken_Multi-Dropper.py
    
    * Update CanaryToken_Multi-Dropper.sh
    
    * Update CanaryToken_Multi-Dropper.sh
    
    * Update CanaryToken_Multi-Dropper.py

--- python/CanaryToken_Multi-Dropper.py
+++ python/CanaryToken_Multi-Dropper.py
@@ -7,57 +7,6 @@ Domain = "XXXXXX.canary.tools" # Enter your Console domain between the . e.g. 12
 FactoryAuth = "XXXXX" # Enter your Factory auth key. e.g a1bc3e769fg832hij3 Docs available here. https://docs.canary.tools/canarytokens/factory.html#create-canarytoken-factory-auth-string
 FlockID = "flock:default" # Enter desired flock ID to place tokens in. Docs available here. https://docs.canary.tools/flocks/queries.html#list-flock-sensors
 
-def drop_msword_token():
-  #Drop MSWord Token 
-  token_directory = os.path.expanduser('~')+'/mswordtoken/' # Download location of token.
-  token_filename = 'secrets.docx' # Token filename, consider an enticing name to attract attackers.
-  token_type = 'doc-msword'
-  
-  #Create Token on Console
-  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
-  
-  payload = {
-  'factory_auth': FactoryAuth,
-  'kind': token_type,
-  'flock_id' : FlockID,
-  'memo': socket.gethostname()+' - '+token_directory+token_filename,
-  }
-  
-  create_token = requests.post(create_token_url, data=payload)
-  
-  if create_token.status_code != requests.codes.ok:
-    print('[!] Creation of '+token_directory+token_filename+' failed.')
-    exit()
-  else:
-    result = create_token.json()
-  
-  canarytoken_id = result['canarytoken']['canarytoken']
-  
-  #Download token to endpoint.
-  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
-  
-  payload = {
-  'factory_auth': FactoryAuth,
-  'canarytoken': canarytoken_id
-  }
-  
-  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
-  
-  if fetch_token.status_code != requests.codes.ok:
-    print('[!] Fetching of '+token_directory+token_filename+' failed.')
-    exit()
-  else:
-    result = create_token.json()
-  
-  if not os.path.exists(token_directory):
-    os.makedirs(token_directory)
-  
-  open(token_directory+token_filename, 'wb').write(fetch_token.content)
-  
-  print("[*] MSWord Token Dropped")
-
-drop_msword_token()
-
 def drop_awsid_token():
   #Drop aws-id Token 
   token_directory = os.path.expanduser('~')+'/awsidtoken/' # Download location of token.
@@ -108,6 +57,114 @@ def drop_awsid_token():
   print("[*] AWS-API Key Token Dropped")
 
 drop_awsid_token()
+
+def drop_azureid_token():
+  #Drop azure-id Token
+  token_directory = os.path.expanduser('~')+'/azureidtoken/'
+  token_filename = 'azure-login.zip'
+  token_type = 'azure-id'
+  AzureCertName = 'prod.pem'
+
+  #Create Token on Console
+  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
+
+  payload = {
+  'factory_auth': FactoryAuth,
+  'kind': token_type,
+  'flock_id' : FlockID,
+  'azure_id_cert_file_name' : AzureCertName,
+  'memo': socket.gethostname()+' - '+token_directory+token_filename,
+  }
+
+  create_token = requests.post(create_token_url, data=payload)
+  
+  if create_token.status_code != requests.codes.ok:
+    print('[!] Creation of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+
+  canarytoken_id = result['canarytoken']['canarytoken']
+
+  #Download token to endpoint.
+  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
+  payload = {
+  'factory_auth': FactoryAuth,
+  'canarytoken': canarytoken_id
+  }
+
+  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
+  
+  if fetch_token.status_code != requests.codes.ok:
+    print('[!] Fetching of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+  
+  if not os.path.exists(token_directory):
+    os.makedirs(token_directory)
+  
+  open(token_directory+token_filename, 'wb').write(fetch_token.content)
+
+  with zipfile.ZipFile(token_directory+token_filename, 'r') as zip_ref:
+    zip_ref.extractall(token_directory)
+  
+  os.remove(token_directory+token_filename)
+
+  print("[*] Azure-ID Token Dropped")
+
+drop_azureid_token()
+
+def drop_msword_token():
+  #Drop MSWord Token 
+  token_directory = os.path.expanduser('~')+'/mswordtoken/' # Download location of token.
+  token_filename = 'secrets.docx' # Token filename, consider an enticing name to attract attackers.
+  token_type = 'doc-msword'
+  
+  #Create Token on Console
+  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
+  
+  payload = {
+  'factory_auth': FactoryAuth,
+  'kind': token_type,
+  'flock_id' : FlockID,
+  'memo': socket.gethostname()+' - '+token_directory+token_filename,
+  }
+  
+  create_token = requests.post(create_token_url, data=payload)
+  
+  if create_token.status_code != requests.codes.ok:
+    print('[!] Creation of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+  
+  canarytoken_id = result['canarytoken']['canarytoken']
+  
+  #Download token to endpoint.
+  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
+  
+  payload = {
+  'factory_auth': FactoryAuth,
+  'canarytoken': canarytoken_id
+  }
+  
+  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
+  
+  if fetch_token.status_code != requests.codes.ok:
+    print('[!] Fetching of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+  
+  if not os.path.exists(token_directory):
+    os.makedirs(token_directory)
+  
+  open(token_directory+token_filename, 'wb').write(fetch_token.content)
+  
+  print("[*] MSWord Token Dropped")
+
+drop_msword_token()
 
 def drop_mswordmacro_token():
   #Drop msword-macro Token 
@@ -262,57 +319,6 @@ def drop_adobepdf_token():
 
 drop_adobepdf_token()
 
-def drop_slackapi_token():
-  #Drop Slack API Token 
-  token_directory = os.path.expanduser('~')+'/slackapitoken/' # Download location of token.
-  token_filename = 'slack_api_creds.txt' # Token filename, consider an enticing name to attract attackers.
-  token_type = 'slack-api'
-  
-  #Create Token on Console
-  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
-  
-  payload = {
-  'factory_auth': FactoryAuth,
-  'kind': token_type,
-  'flock_id' : FlockID,
-  'memo': socket.gethostname()+' - '+token_directory+token_filename,
-  }
-  
-  create_token = requests.post(create_token_url, data=payload)
-  
-  if create_token.status_code != requests.codes.ok:
-    print('[!] Creation of '+token_directory+token_filename+' failed.')
-    exit()
-  else:
-    result = create_token.json()
-  
-  canarytoken_id = result['canarytoken']['canarytoken']
-  
-  #Download token to endpoint.
-  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
-  
-  payload = {
-  'factory_auth': FactoryAuth,
-  'canarytoken': canarytoken_id
-  }
-  
-  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
-  
-  if fetch_token.status_code != requests.codes.ok:
-    print('[!] Fetching of '+token_directory+token_filename+' failed.')
-    exit()
-  else:
-    result = create_token.json()
-  
-  if not os.path.exists(token_directory):
-    os.makedirs(token_directory)
-  
-  open(token_directory+token_filename, 'wb').write(fetch_token.content)
-  
-  print("[*] Slack API Token Dropped")
-
-drop_slackapi_token()
-
 def drop_qr_code_token():
   #Drop QR-Code Token 
   token_directory = os.path.expanduser('~')+'/qrcodetoken/' # Download location of token.
@@ -363,6 +369,57 @@ def drop_qr_code_token():
   print("[*] QR Code Token Dropped")
 
 drop_qr_code_token()
+
+def drop_slackapi_token():
+  #Drop Slack API Token 
+  token_directory = os.path.expanduser('~')+'/slackapitoken/' # Download location of token.
+  token_filename = 'slack_api_creds.txt' # Token filename, consider an enticing name to attract attackers.
+  token_type = 'slack-api'
+  
+  #Create Token on Console
+  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
+  
+  payload = {
+  'factory_auth': FactoryAuth,
+  'kind': token_type,
+  'flock_id' : FlockID,
+  'memo': socket.gethostname()+' - '+token_directory+token_filename,
+  }
+  
+  create_token = requests.post(create_token_url, data=payload)
+  
+  if create_token.status_code != requests.codes.ok:
+    print('[!] Creation of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+  
+  canarytoken_id = result['canarytoken']['canarytoken']
+  
+  #Download token to endpoint.
+  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
+  
+  payload = {
+  'factory_auth': FactoryAuth,
+  'canarytoken': canarytoken_id
+  }
+  
+  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
+  
+  if fetch_token.status_code != requests.codes.ok:
+    print('[!] Fetching of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+  
+  if not os.path.exists(token_directory):
+    os.makedirs(token_directory)
+  
+  open(token_directory+token_filename, 'wb').write(fetch_token.content)
+  
+  print("[*] Slack API Token Dropped")
+
+drop_slackapi_token()
 
 def drop_windows_folder_token():
   #Drop Windows Folder Token 
@@ -419,3 +476,53 @@ def drop_windows_folder_token():
   print("[*] Windows Folder Token Dropped")
 
 drop_windows_folder_token()
+
+def drop_wireguardvpn_token():
+  #Drop azure-id Token
+  token_directory = os.path.expanduser('~')+'/wireguardvpntoken/'
+  token_filename = 'wg0.conf'
+  token_type = 'wireguard'
+
+  #Create Token on Console
+  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
+
+  payload = {
+  'factory_auth': FactoryAuth,
+  'kind': token_type,
+  'flock_id' : FlockID,
+  'memo': socket.gethostname()+' - '+token_directory+token_filename,
+  }
+
+  create_token = requests.post(create_token_url, data=payload)
+  
+  if create_token.status_code != requests.codes.ok:
+    print('[!] Creation of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+
+  canarytoken_id = result['canarytoken']['canarytoken']
+
+  #Download token to endpoint.
+  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
+  payload = {
+  'factory_auth': FactoryAuth,
+  'canarytoken': canarytoken_id
+  }
+
+  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
+  
+  if fetch_token.status_code != requests.codes.ok:
+    print('[!] Fetching of '+token_directory+token_filename+' failed.')
+    exit()
+  else:
+    result = create_token.json()
+  
+  if not os.path.exists(token_directory):
+    os.makedirs(token_directory)
+  
+  open(token_directory+token_filename, 'wb').write(fetch_token.content)
+
+  print("[*] Wireguard VPN Token Dropped")
+
+drop_wireguardvpn_token()
