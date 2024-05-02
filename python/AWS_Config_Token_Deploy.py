912111141fe966f07b68ad999832c8846981517e
Mr Forest <1726833+0xForest@users.noreply.github.com>
2024-05-02 20:39:21 +0200

    Update AWS_Config_Token_Deploy.py

--- python/AWS_Config_Token_Deploy.py
+++ python/AWS_Config_Token_Deploy.py
@@ -1,62 +1,50 @@
-import requests
+import urllib.request
 import socket
 import os
+import json
 
 Domain = "ABC123.canary.tools" # Enter your Console domain between the . e.g. 1234abc.canary.tools
 FactoryAuth = "ABC123" # Enter your Factory auth key. e.g a1bc3e769fg832hij3 Docs available here. https://docs.canary.tools/canarytokens/factory.html#create-canarytoken-factory-auth-string
-FlockID = "flock:default" # Enter desired flock ID to place tokens in. Docs available here. https://docs.canary.tools/flocks/queries.html#list-flock-sensors
 
 def drop_awsid_token():
-  #Drop aws-id Token 
-  token_directory = os.path.expanduser('~')+'/.aws/' # Download location of token.
-  token_filename = 'config' # Token filename, consider an enticing name to attract attackers.
-  token_type = 'aws-id'
-  
-  #Create Token on Console
-  create_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/create'
-  
-  payload = {
-  'factory_auth': FactoryAuth,
-  'kind': token_type,
-  'flock_id' : FlockID,
-  'memo': socket.gethostname()+' - '+token_directory+token_filename,
-  }
-  
-  create_token = requests.post(create_token_url, data=payload)
-  
-  if create_token.status_code != requests.codes.ok:
-    print('[!] Creation of '+token_directory+token_filename+' failed.')
-    exit()
-  else:
-    result = create_token.json()
-  
-  canarytoken_id = result['canarytoken']['canarytoken']
-  
-  #Download token to endpoint.
-  download_token_url = 'https://'+Domain+'/api/v1/canarytoken/factory/download'
-  
-  payload = {
-  'factory_auth': FactoryAuth,
-  'canarytoken': canarytoken_id
-  }
-  
-  fetch_token = requests.get(download_token_url, allow_redirects=True, params=payload)
-  
-  if fetch_token.status_code != requests.codes.ok:
-    print('[!] Fetching of '+token_directory+token_filename+' failed.')
-    exit()
-  else:
-    result = create_token.json()
+    # Create Token on Console
+    create_token_url = 'https://' + Domain + '/api/v1/canarytoken/factory/create'
 
-  if not os.path.exists(token_directory):
-    os.makedirs(token_directory)
-  
-  if os.path.exists(token_directory+token_filename):
-    open(token_directory+token_filename, 'a').write("\n")
-    open(token_directory+token_filename, 'ab').write(fetch_token.content)
-  else:
-    open(token_directory+token_filename, 'wb').write(fetch_token.content)
+    payload = {
+        'factory_auth': FactoryAuth,
+        'kind': 'aws-id',
+        'memo': socket.gethostname() + ' - ' + 'Inserted into ~/.aws/credentials',
+    }
 
-  print("[*] AWS-API Key Token Dropped")
+    data = urllib.parse.urlencode(payload).encode("utf-8")
+    req = urllib.request.Request(create_token_url, data=data)
+
+    with urllib.request.urlopen(req) as create_token:
+        if create_token.status != 200:
+            print('[!] Creation of AWS Token failed.')
+            exit()
+        else:
+            result = json.loads(create_token.read().decode('utf-8'))
+
+    canarytoken_key_id = result['canarytoken']['access_key_id']
+    canarytoken_access_key = result['canarytoken']['secret_access_key']
+
+    # Insert into AWS CLI Configuration
+    aws_profile_name = "emergency-admin-creds"  # Profile name for the AWS CLI configuration
+    aws_config_file = os.path.expanduser("~/.aws/config")
+
+    # Construct the AWS CLI configuration content
+    config_content = f"""
+[profile {aws_profile_name}]
+region = us-west-2
+aws_access_key_id = {canarytoken_key_id}
+aws_secret_access_key = {canarytoken_access_key}
+"""
+
+    # Write the configuration content to the AWS config file
+    with open(aws_config_file, "a") as config_file:
+        config_file.write(config_content)
+
+    print(f"AWS profile '{aws_profile_name}' configured successfully.")
 
 drop_awsid_token()
