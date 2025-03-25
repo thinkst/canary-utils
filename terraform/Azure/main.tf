de31d03c77f04177dc88d67ff03552136f2f9063
Mr Forest <1726833+0xForest@users.noreply.github.com>
2025-03-25 13:31:02 +0200

    Updates Azure terraform deployment template (#46)
    This update changes how we handle deployment by instead fetching the required details from your Console.
    
    This is more inline with an automated approach as the Canary service principle credentials will change over time.

--- terraform/Azure/main.tf
+++ terraform/Azure/main.tf
@@ -1,87 +1,126 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "= 3.21.1"
-    }
-  }
+##########################
+# 🔧 USER CONFIGURATION  #
+##########################
 
-  required_version = ">= 1.1.0"
+# Please replace the values below with your own.
+# Also check line 101 for security group configuration.
+
+variable "user_config" {
+  type = map(string)
+  default = {
+    api_key        = "ab...cd" # We use this to fetch the details of your Azure Canary image from your Console automatically. This can be a read only API Key.
+    console_hash   = "ab...cd"
+    location = "US East"
+    resource_group_name = "birdy-rg"
+    subscription_id = "ab...cd"
+    vm_name = "birdy-vm"
+    vnet_name = "birdy-vnet"
+    vnet_address_space = "10.0.0.0/16"
+    subnet_name = "birdy-subnet"
+    subnet_prefixes = "10.0.1.0/24"
+    nic_name = "birdy-NIC"
+    nic_config_name = "birdy-NIC-Config"
+    nsg_name = "birdy-NSG"
+  }
+  description = "Your configuration for the Canary deployment"
 }
 
-# Authenticate to Azure as Canary app. Below specifies what fields are needed. 
-# Consider safer authentication methods for production use as documented by Hashicorp: 
-#   https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_client_secret
+# Fetch the license details from the Console API with a Read-Only API Key.
+data "http" "license" {
+  url = "https://${var.user_config.console_hash}.canary.tools/api/v1/license/detailed/info"
+
+  request_headers = {
+    "X-Canary-Auth-Token" = var.user_config.api_key
+  }
+}
+
+# Extracts details of your Canary App.
+locals {
+  license_info              = jsondecode(data.http.license.response_body)
+  azure_app_password        = local.license_info.azurecanary_details.azurecanary_app_password
+  azure_app_id              = local.license_info.azurecanary_details.azurecanary_app_id
+  azure_app_tenant          = local.license_info.azurecanary_details.azurecanary_thinkst_tenant_id
+  azure_app_image_details   = local.license_info.azurecanary_details.azurecanary_image_details
+  azure_app_latest_image    = local.azure_app_image_details[length(local.azure_app_image_details) - 1]
+  azure_app_latest_image_id = local.azure_app_latest_image.id
+  azure_customer_tenant     = local.license_info.azurecanary_details.azurecanary_customer_tenant_id # This will be the default Tenant ID configured on your Console, please override if you are deploying to a different tenant.
+}
+
+output "azurecanary_app_details" {
+  value = {
+    app_id          = local.azure_app_id
+    app_password    = local.azure_app_password
+    app_tenant      = local.azure_app_tenant
+    customer_tenant = local.azure_customer_tenant
+  }
+}
+
 provider "azurerm" {
   features {}
-  
-  subscription_id = "<YOUR SUBSCRIPTION ID>"
-  client_id       = "<YOUR CANARY APP SERVICE ACCOUNT USERNAME>"
-  client_secret   = "<YOUR CANARY APP SERVICE ACCOUNT PASSWORD>"
-  tenant_id       = "YOUR TENANT ID"
-  auxiliary_tenant_ids = ["TENANT ID OF CANARY APP SERVICE ACCOUNT"]
+  subscription_id      = var.user_config.subscription_id
+  client_id            = local.azure_app_id
+  client_secret        = local.azure_app_password
+  tenant_id            = local.azure_customer_tenant
+  auxiliary_tenant_ids = [local.azure_app_tenant]
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "main" {
-  name     = "<DESTINATION RESOURCE GROUP>"
-  location = "<DESTINATION REGION>"
+# Resource Group
+resource "azurerm_resource_group" "birdy" {
+  name     = var.user_config.resource_group_name
+  location = var.user_config.location
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet1"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+# Virtual Network
+resource "azurerm_virtual_network" "birdy" {
+  name                = var.user_config.vnet_name
+  location            = var.user_config.location
+  resource_group_name = azurerm_resource_group.birdy.name
+  address_space       = [var.user_config.vnet_address_space]
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+# Subnet
+resource "azurerm_subnet" "birdy" {
+  name                 = var.user_config.subnet_name
+  resource_group_name  = azurerm_resource_group.birdy.name
+  virtual_network_name = azurerm_virtual_network.birdy.name
+  address_prefixes     = [var.user_config.subnet_prefixes]
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "mybird_nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+# Network Interface
+resource "azurerm_network_interface" "birdy" {
+  name                = var.user_config.nic_name
+  location            = var.user_config.location
+  resource_group_name = azurerm_resource_group.birdy.name
 
   ip_configuration {
-    name                          = "mybirdip"
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = var.user_config.nic_config_name
+    subnet_id                     = azurerm_subnet.birdy.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
-  name                = "mybird"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  vm_size               = "Standard_B1ls"
-  network_interface_ids = [azurerm_network_interface.main.id]
-  delete_os_disk_on_termination = true
-  delete_data_disks_on_termination = true
+# If you would like an existing NSG attached to the NIC, please uncomment the following block and replace the values with your own.
+#resource "azurerm_network_interface_security_group_association" "birdy" {
+#  network_interface_id      = azurerm_network_interface.birdy.id
+#  network_security_group_id = "/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers/Microsoft.Network/networkSecurityGroups/{nsg_name}"
+#}
 
-  storage_image_reference {
-    id = "/subscriptions/<AZURE IMAGE GALLERY LOCATION>/images/AzureCanary-3.6.2-0f374ec"
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "birdy" {
+  name                = var.user_config.vm_name
+  resource_group_name = azurerm_resource_group.birdy.name
+  location            = var.user_config.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "unused"
+  admin_password      = "ThisIsNotARealPasswordAndWillGoUnused-d9fcdb312f0cb8c593c91a0f7d4a118f"
+  disable_password_authentication = false
+
+  network_interface_ids = [azurerm_network_interface.birdy.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_os_disk {
-    name              = "mybird_os"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "mybird"
-    admin_username = "notused"
-    admin_password = "Password123"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+  source_image_id = local.azure_app_latest_image_id
 }
